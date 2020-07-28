e2b7ef7c4c44bf1d018feacdb71dc19eeaab2d28
Atharva Nadkar <67808562+Atharva-Nadkar@users.noreply.github.com>
2020-07-28 22:05:19 +0530

    Create question 2.cpp

---
+++ question 2.cpp
@@ -0,0 +1,26 @@
+//  Created by Atharva Nadkar on 21/06/20.
+//  Copyright © 2020 Atharva_Nadkar. All rights reserved.
+//  Solution to question 2.
+
+#include<algorithm>
+#include<stdio.h>
+#include<iostream>
+#include<string>
+#include<stdio.h>
+#include<math.h>
+#include<sstream>
+
+using namespace std;
+ 
+int main()
+{
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    
+    float a,b;
+    cout<<"Enter two numbers: ";
+    cin>>a>>b;
+    cout<<"product = "<<a*b<<endl;
+    
+    return 0;
+}
