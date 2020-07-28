4703f9407056a1ee5d9a9d3c47de522b36fdb043
Atharva Nadkar <67808562+Atharva-Nadkar@users.noreply.github.com>
2020-07-28 22:00:57 +0530

    Create question 1.cpp

---
+++ question 1.cpp
@@ -0,0 +1,31 @@
+//  Created by Atharva Nadkar on 21/06/20.
+//  Copyright © 2020 Atharva_Nadkar. All rights reserved.
+//  Solution to question 1.
+
+#include<algorithm>
+#include<stdio.h>
+#include<iostream>
+#include<string>
+#include<stdio.h>
+#include<math.h>
+#include<sstream>
+
+using namespace std;
+ 
+int main()
+{
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    
+    char c;
+    int i;
+    float f;
+    double d;
+    
+    cout <<"Size of char: "<<sizeof(c)<<" byte"<<endl;
+    cout <<"Size of int: "<<sizeof(i)<<" bytes"<<endl;
+    cout <<"Size of float: "<<sizeof(f)<<" bytes"<<endl;
+    cout <<"Size of double: "<<sizeof(d)<<" bytes"<<endl;
+    
+    return 0;
+}
